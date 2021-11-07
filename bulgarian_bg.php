b0eebaa85466fd97709538d5c75c41c30a9a0667
GEORGE <73117789+lz1gsp@users.noreply.github.com>
2021-11-07 22:52:20 +0200

    Add files via upload

---
+++ bulgarian_bg.php
@@ -0,0 +1,154 @@
+<?php
+//
+// English US Language Pack
+// Andy Taylor (MW0MWZ)
+// Updated: 02-Aug-2017
+//
+// Български BG Езиков пакет
+// Жорж Паликарски (LZ1GSP)
+// Актуализиран: 12-Авг-2020
+//
+$lang = array (
+  // Banner texts
+  "digital_voice"               =>  "Хотспот",
+  "configuration"               =>  "Настройки",
+  "dashboard_for"               =>  "Дашборд на",
+  // Banner links
+  "dashboard"                   =>  "Дашборд",
+  "admin"                       =>  "Админ",
+  "power"                       =>  "Захранване",
+  "update"                      =>  "Обновяване",
+  "backup_restore"              =>  "Архивиране/Възстановяване",
+  "factory_reset"               =>  "Начално състояние",
+  "live_logs"                   =>  "Живи данни",
+  // Config page section headdings
+  "hardware_info"               =>  "Инфо хардуер",
+  "control_software"            =>  "Софтуерен контрол",
+  "mmdvmhost_config"            =>  "MMDVMHost Настройки",
+  "general_config"              =>  "Главни настройки",
+  "dmr_config"                  =>  "DMR Настройки",
+  "dstar_config"                =>  "D-Star Настройки",
+  "ysf_config"                  =>  "YSF Настройки",
+  "p25_config"                  =>  "P25 Настройки",
+  "nxdn_config"                 =>  "NXDN Настройки",
+  "pocsag_config"               =>  "POCSAG Настройки",
+  "mobilegps_config"            =>  "Мобилни GPS Настройки",
+  "wifi_config"                 =>  "WiFi Настройки",
+  "fw_config"                   =>  "Firewall Настройки",
+  "remote_access_pw"            =>  "Парола за отдалечен достъп",
+  // Config Page - Section General
+  "setting"                     =>  "Настройки",
+  "value"                       =>  "Стойности",
+  "apply"                       =>  "Приложи промените",
+  // Config Page - Gateway Hardware Information
+  "hostname"                    =>  "Име на хоста",
+  "kernel"                      =>  "Ядро",
+  "platform"                    =>  "Платформа",
+  "cpu_load"                    =>  "Натоварване на CPU",
+  "cpu_temp"                    =>  "Температура на CPU",
+  // Config Page - Control Software
+  "controller_software"         =>  "Софтуер на Контролера",
+  "controller_mode"             =>  "Режим на Контролера",
+  // Config Page - MMDVMHost Configuration
+  "dmr_mode"                    =>  "DMR Режим",
+  "d-star_mode"                 =>  "D-Star Режим",
+  "ysf_mode"                    =>  "YSF Режим",
+  "p25_mode"                    =>  "P25 Режим",
+  "nxdn_mode"                   =>  "NXDN Режим",
+  "mmdvm_display"               =>  "MMDVM Вид на дисплея",
+  "mode_hangtime"               =>  "Режим на прекъсване",
+  // Config Page - General Configuration
+  "node_call"                   =>  "Инициал",
+  "dmr_id"                      =>  "CCS7/DMR ID",
+  "radio_freq"                  =>  "Честота на радиото",
+  "lattitude"                   =>  "Ширина",
+  "longitude"                   =>  "Дължина",
+  "town"                        =>  "Град",
+  "country"                     =>  "Държава",
+  "url"                         =>  "URL",
+  "radio_type"                  =>  "Радио/Модем Тип",
+  "node_type"                   =>  "Тип на връзката",
+  "timezone"                    =>  "Системно време",
+  "dash_lang"                   =>  "Език на Дашборда",
+  // Config Page - DMR Configuration
+  "dmr_master"                  =>  "DMR Мастер (MMDVMHost)",
+  "bm_master"                   =>  "BrandMeister Мастер",
+  "bm_network"                  =>  "BrandMeister Мрежа",
+  "dmr_plus_master"             =>  "DMR+ Мастер",
+  "dmr_plus_network"            =>  "DMR+ Мрежа",
+  "xlx_master"                  =>  "XLX Мастер",
+  "xlx_enable"                  =>  "XLX Мастер Активиране",
+  "dmr_cc"                      =>  "DMR Колор код",
+  "dmr_embeddedlconly"          =>  "DMR Достъпен само локално",
+  "dmr_dumptadata"              =>  "DMR Отхвърляне на TA Данни",
+  // Config Page - D-Star Configuration
+  "dstar_rpt1"                  =>  "РПТ1 Знак",
+  "dstar_rpt2"                  =>  "РПТ2 Знак",
+  "dstar_irc_password"          =>  "Дистанционна парола",
+  "dstar_default_ref"           =>  "Рефлектор по подразбиране",
+  "aprs_host"                   =>  "APRS Хост",
+  "dstar_irc_lang"              =>  "Език на ircDDBGateway",
+  "dstar_irc_time"              =>  "Времеви анонс",
+  // Config Page - YSF Configuration
+  "ysf_startup_host"            =>  "YSF Стартов хост",
+  // Config Page - P25 Configuration
+  "p25_startup_host"            =>  "P25 Стартов хост",
+  "p25_nac"                     =>  "P25 NAC",
+  // Config Page - NXDN Configuration
+  "nxdn_startup_host"            =>  "NXDN Стартов хост",
+  "nxdn_ran"                     =>  "NXDN RAN",
+  // Config Page - MobileGPS Configuration
+  "mobilegps_enable"            =>  "Включване на МобиленGPS",
+  "mobilegps_port"              =>  "GPS Порт",
+  "mobilegps_speed"             =>  "Скорост на GPS порта",
+  // Config Page - Firewall Configuration
+  "fw_dash"                     =>  "Достъп до Дашборда",
+  "fw_irc"                      =>  "ircDDBGateway дист. контрол",
+  "fw_ssh"                      =>  "SSH достъп",
+  // Config Page - Password
+  "user"                        =>  "Потребителско име",
+  "password"                    =>  "Парола",
+  "set_password"                =>  "Избиране на парола",
+  // Dashboard Front Page - Repeater Info Pannel
+  "modes_enabled"               =>  "Активни режими",
+  "net_status"                  =>  "Състояние на мрежата",
+  "internet"                    =>  "Интернет",
+  "radio_info"                  =>  "Радио Инфо",
+  "dstar_repeater"              =>  "D-Star Репитер",
+  "dstar_net"                   =>  "D-Star Мрежа",
+  "dmr_repeater"                =>  "DMR Репитер",
+  "dmr_master"                  =>  "DMR Мастер",
+  "ysf_net"                     =>  "YSF Мрежа",
+  "p25_radio"                   =>  "P25 Радио",
+  "p25_net"                     =>  "P25 Мрежа",
+  "nxdn_radio"                  =>  "NXDN Радио",
+  "nxdn_net"                    =>  "NXDN Мрежа",
+  // Dashboard Front Page - Calls
+  "time"                        =>  "Време",
+  "mode"                        =>  "Режим",
+  "callsign"                    =>  "Инициал",
+  "target"                      =>  "Цел",
+  "src"                         =>  "Изт",        // Съкращение на "Източник"
+  "dur"                         =>  "Прод",        // Съкращение на "Продължителност"
+  "loss"                        =>  "Загуби",
+  "ber"                         =>  "BER",        // Съкращение на "Брой грешки"
+  // POCSAG Specific
+  "pocsag_list"                 =>  "DAPNET Gateway Активност",
+  "pocsag_timeslot"             =>  "Времеви слот",
+  "pocsag_msg"                  =>  "Съобщение",
+  // Dashboard - Extra Info
+  "group"                       =>  "Група",
+  "logoff"                      =>  "Отписване",
+  "info"                        =>  "Информация",
+  "utot"                        =>  "ПTOT",       // Съкращение на Потребителси таймаут
+  "gtot"                        =>  "ГTOT",       // Съкращение на Групов таймаут
+  // Dashboard Front Page / Admin - Section Headders
+  "last_heard_list"             =>  "Gateway Активност",
+  "local_tx_list"               =>  "Локална радио активност",
+  "active_starnet_groups"       =>  "Активност на Starnet Групите",
+  "active_starnet_members"      =>  "Активни Starnet Групови членове",
+  "d-star_link_manager"         =>  "D-Star Менажер на връзките",
+  "d-star_link_status"          =>  "D-Star Инфо за връзките",
+  "service_status"              =>  "Сервизно състояние"
+);
+?>
